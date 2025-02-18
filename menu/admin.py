7b4913711e05493b9bccac8117af6717c2aede29
Angelo-Santiago <angelosantiago410@gmail.com>
2025-02-17 21:01:00 -0300

    implementando admin

--- menu/admin.py
+++ menu/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Category, Item
 
-# Register your models here.
+# Registra o modelo Category no Django Admin
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'descricao']  # Exibe essas colunas na lista
+    search_fields = ['nome']  # Permite pesquisar por nome da categoria
+
+admin.site.register(Category, CategoryAdmin)
+
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['nome', 'preco', 'categoria', 'descricao']  # Exibe essas colunas na lista
+    search_fields = ['nome', 'descricao']  # Permite pesquisar por nome ou descrição
+    list_filter = ['categoria']  # Filtra por categoria
+    ordering = ['nome']  # Ordena por nome
+    list_per_page = 10  # Limita o número de itens exibidos por página no admin
+
+admin.site.register(Item, ItemAdmin)
--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,15 @@
-from django.contrib import admin
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import Table
+
+# Admin para o modelo Table
+class TableAdmin(admin.ModelAdmin):
+    list_display = ['numero', 'capacidade']  # Exibe as colunas 'numero' e 'capacidade'
+    search_fields = ['numero']  # Permite pesquisa pelo número da mesa
+    list_filter = ['capacidade']  # Filtra mesas pela capacidade
+    ordering = ['numero']  # Ordena as mesas pelo número
+    list_per_page = 10  # Exibe 10 itens por página na listagem
+
+# Registra o modelo Table no Django Admin com a configuração personalizada
+admin.site.register(Table, TableAdmin)
